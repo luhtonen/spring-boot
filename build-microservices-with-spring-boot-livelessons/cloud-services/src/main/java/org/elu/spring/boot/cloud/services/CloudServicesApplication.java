35df12aaf9b5c2aed6e1a7c844fe5050e447c936
luhtonen <luhtonen@gmail.com>
2016-04-08 18:20:05 +0200

    Spring Boot application to consume services
    Building Microservices with Spring Boot LiveLessons: Spring boot
    application which consumes services with multiple database services
    defined to demonstrate how to select one service.

---
+++ build-microservices-with-spring-boot-livelessons/cloud-services/src/main/java/org/elu/spring/boot/cloud/services/CloudServicesApplication.java
@@ -0,0 +1,12 @@
+package org.elu.spring.boot.cloud.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServicesApplication.class, args);
+	}
+}
