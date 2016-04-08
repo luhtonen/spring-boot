0d89add8bba78b9f7c13cdc710376cf57d120b39
luhtonen <luhtonen@gmail.com>
2016-04-08 11:53:57 +0200

    Sample Spring Boot Initializr project with IntelliJ IDEA
    Building Microservices with Spring Boot LiveLessons: example Spring
    Boot application created in IntelliJ IDEA as new Spring Initializr
    project

---
+++ build-microservices-with-spring-boot-livelessons/demo2/src/main/java/org/elu/sping/boot/example/Demo2Application.java
@@ -0,0 +1,12 @@
+package org.elu.sping.boot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2Application.class, args);
+	}
+}
