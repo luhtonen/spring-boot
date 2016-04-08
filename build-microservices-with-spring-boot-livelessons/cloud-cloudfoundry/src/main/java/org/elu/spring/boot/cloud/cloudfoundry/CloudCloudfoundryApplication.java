0a82ab63383fb583624576504a7b1daac84bde29
luhtonen <luhtonen@gmail.com>
2016-04-08 15:24:58 +0200

    Demo Spring Boot application for Cloud Foundry
    Building Microservices with Spring Boot LiveLessons: Deploy Spring Boot
    application to Cloud Foundry

---
+++ build-microservices-with-spring-boot-livelessons/cloud-cloudfoundry/src/main/java/org/elu/spring/boot/cloud/cloudfoundry/CloudCloudfoundryApplication.java
@@ -0,0 +1,12 @@
+package org.elu.spring.boot.cloud.cloudfoundry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCloudfoundryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudCloudfoundryApplication.class, args);
+	}
+}
---
+++ build-microservices-with-spring-boot-livelessons/cloud-cloudfoundry/src/test/java/org/elu/spring/boot/cloud/cloudfoundry/CloudCloudfoundryApplicationTests.java
@@ -0,0 +1,16 @@
+package org.elu.spring.boot.cloud.cloudfoundry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CloudCloudfoundryApplication.class)
+public class CloudCloudfoundryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
